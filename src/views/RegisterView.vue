a2cc10eedde947d1d350e9c88aa3b5d59f6afb20
jayhong329 <smoja329@gmail.com>
2024-10-21 13:51:09 +0800

    註冊驗證練習

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -3,22 +3,23 @@
         <div class="col-3"></div>
         <div class="col-6">
             <h3>會員註冊</h3>
-            <form id="registerForm" novalidate>
+            <!-- 不想用內建的驗證  就加上"novalidate" -->
+            <form id="registerForm" novalidate @submit.prevent="validate">
                 <div class="input-group">
                     <label for="account" class="input-group-text">帳號</label>
-                    <input type="text" class="form-control" id="account" required autofocus autocomplete="off">
-                    <span class="input-group-text bg-danger text-white"><i class="bi bi-x-lg"></i></span>
+                    <input type="text" v-model="userData.name" class="form-control" id="account" required autofocus autocomplete="off">
+                    <span v-show="!validity.nameRequired" class="input-group-text bg-danger text-white"><i class="bi bi-x-lg"></i></span>
                 </div>
-                <div class="mb-3"><small class="text-danger">帳號一定要輸入</small></div>
+                <div class="mb-3"><small v-if="!validity.nameRequired" class="text-danger">帳號一定要輸入</small></div>
                 <div class="input-group">
                     <label for="pwd1" class="input-group-text">密碼</label>
-                    <input type="password" class="form-control" id="pwd1" autocomplete="off">
-                    <span class="input-group-text bg-danger text-white"><i class="bi bi-x-lg"></i></span>
+                    <input type="password" v-model="userData.pwd1" class="form-control" id="pwd1" autocomplete="off">
+                    <span v-show="!validity.pwdRequired" class="input-group-text bg-danger text-white"><i class="bi bi-x-lg"></i></span>
                 </div>
-                <div class="mb-3"><small class="text-danger">密碼一定要輸入</small></div>
+                <div class="mb-3"><small v-if="!validity.pwdRequired" class="text-danger">密碼一定要輸入</small></div>
                 <div class="input-group">
                     <label for="pwd2" class="input-group-text">密碼確認</label>
-                    <input type="password" class="form-control" id="pwd2" autocomplete="off">
+                    <input type="password" v-model="userData.pwd2" class="form-control" id="pwd2" autocomplete="off">
                     <span class="input-group-text bg-danger text-white hide"><i class="bi bi-x-lg"></i></span>
                 </div>
                 <div class="mb-3">
@@ -26,11 +27,11 @@
                 </div>
                 <div class="input-group">
                     <label for="email" class="input-group-text">電子郵件</label>
-                    <input type="email" class="form-control" id="email">
-                    <span class="input-group-text bg-danger text-white"><i class="bi bi-x-lg"></i></span>
+                    <input type="email" v-model="userData.email" class="form-control" id="email">
+                    <span v-show="!validity.emailRequired" class="input-group-text bg-danger text-white"><i class="bi bi-x-lg"></i></span>
                 </div>
                 <div class="mb-3">
-                    <small class="text-danger">電子郵件要輸入</small><br>
+                    <small v-if="!validity.emailRequired" class="text-danger">電子郵件要輸入</small><br>
                     <small class="text-danger">電子郵件格式不正確</small>
                 </div>
                 <button type="submit" class="btn btn-primary" id="buttonSubmit">送出</button>
@@ -42,5 +43,45 @@
 </template>
 <style></style>
 <script setup>
+import { ref } from 'vue';
+// 兩個// = new Regexp()
 const emailRule = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+
+const userData = ref({
+    "name":"",
+    "pwd1":"",
+    "pwd2":"",
+    "email":"",
+})
+
+const validity = ref({
+    "nameRequired":true,
+    "pwdRequired":true,
+    "emailRequired":true,
+    "pwdComfirm":true,
+    "emailFormat":true,
+    "isValid" : false
+})
+
+const validate = () =>{
+    // 資料一定要輸入驗證
+    validity.value.nameRequired = userData.value.name.length > 0
+    validity.value.pwdRequired = userData.value.pwd1.length > 0
+    validity.value.emailRequired = userData.value.email.length > 0
+
+    // 密碼跟密碼確認要一致
+    validity.value.pwdComfirm = userData.value.pwd1 === userData.value.pwd2
+
+    // email 格式檢查
+    validity.value.emailFormat = emailRule.test(userData.value.email)
+
+
+    validity.value.isValid = validity.value.nameRequired && validity.value.pwdRequired && validity.value.emailRequired && validity.value.pwdComfirm && validity.value.emailFormat 
+
+    // ajax 驗證成功
+    if(validity.value.isValid){
+        alert("送出成功")
+    }
+    console.log(userData.value)
+}
 </script>
