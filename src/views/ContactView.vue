9372d635a98bc96b5138e6bdc1b90314f96355a6
jayhong329 <smoja329@gmail.com>
2024-10-22 16:21:52 +0800

    pinia複習

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,6 +1,10 @@
 <script setup>
 import ChildComponent from '@/components/ChildComponent.vue';
+import { useCounterStore } from '@/stores/counter';
 import { ref } from 'vue';
+
+    const storeCounter = useCounterStore()
+
     const abcd = ref("XZY")
     const parentData = ref("")
 
@@ -18,6 +22,12 @@ import { ref } from 'vue';
         message title 是自訂的屬性名稱  用來傳遞資料到子組件
         myEvent 是自訂的事件名稱   目前不知道何時會觸發此事件 -->
         <ChildComponent message="Hello WORLD!!!" :title="abcd" @myEvent="myHandler"></ChildComponent>
+        <hr>
+        <h2>資料來自 pinia store</h2>
+        <input type="number" v-model="storeCounter.count">
+        <span>{{ storeCounter.count }}</span>
+        <span>{{ storeCounter.doubleCount }}</span>
+        <button @click="storeCounter.add">add</button>
     </div>
 </template>
 
