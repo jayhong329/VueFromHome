a2209559b09bdb149b816886ebbecdace3044578
jayhong329 <smoja329@gmail.com>
2024-10-25 21:34:43 +0800

    串接API - 透過 POST 新增

--- src/views/CategoryCRUD.vue
+++ src/views/CategoryCRUD.vue
@@ -5,13 +5,42 @@ import { ref } from 'vue';
     // const API_URL = 'http://127.0.0.1:8000/api/category/'
 
     const categories = ref([])
+    const category = ref({
+        categoryid:0,
+        categoryname:""
+    })
 
     const loadCategories = async () =>{
         const response = await fetch(API_URL)
         const datas = await response.json()
-        console.log(datas)
+        // console.log(datas)
         categories.value = datas
-        console.log(categories.value)
+        // console.log(categories.value)
+    }
+
+    // 串接API - 透過 POST 新增
+    const addCategory = async() =>{
+        // console.log(category.value)
+        const response = await fetch(API_URL,{
+            method: "POST",
+            body: JSON.stringify(category.value),
+            headers: {"Content-Type":"application/json"}
+        })
+        if(response.ok){
+            clearCategory()
+            loadCategories()
+        }else{
+            alert("新增失敗")
+        }
+
+
+    }
+    // 清除 新增欄位資料
+    const clearCategory = () =>{
+        category.value = {
+            categoryid:0,
+            categoryname:""
+        }
     }
     loadCategories()
      
@@ -34,7 +63,12 @@ import { ref } from 'vue';
   
 </ul>
     </div>
-    <div class="col-4"></div>
+    <div class="col-4">
+        <div class="input-group mb-3">
+        <input type="text" v-model="category.categoryname" class="form-control" placeholder="請輸入景點分類">
+        <button @click="addCategory" class="btn btn-outline-secondary" type="button" id="button-addon2">送出</button>
+        </div>
+    </div>
     <div class="col-4"></div>
  </div>
     </div>
