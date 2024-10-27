32dc28c2ece839bf91bbc5c2ee2f98ba37fb3713
jayhong329 <smoja329@gmail.com>
2024-10-27 16:47:10 +0800

    練習-分頁功能

---
+++ src/components/pagingComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+    const props = defineProps({
+        totalPages:Number,
+    })
+    const emit = defineEmits(['goPaging'])
+
+    const clickHandler = page =>{
+        emit("goPaging", page)
+    }
+</script>
+
+<template>
+    <nav aria-label="...">
+  <ul class="pagination pagination-lg">
+    <li @click="clickHandler(page)" class="page-item" v-for="(page, idx) in totalPages " :key="page" >
+      <span class="page-link">{{ page }}</span>
+    </li>
+  </ul>
+</nav>
+</template>
+
+<style lang="css" scoped>
+
+</style>
--- src/views/SpotsView.vue
+++ src/views/SpotsView.vue
@@ -1,32 +1,61 @@
 <script setup>
-import { ref } from 'vue';
+import PagingComponent from '@/components/pagingComponent.vue';
+import { ref, watchEffect } from 'vue';
 
     // 串接API Ajax
     // fetch("http://127.0.0.1:8000/api/spots/")
     //     .then (response => response.json())
     //     .then (datas => console.log(datas))
 
-    const results = ref([])
+    // 資料的搜尋.分頁.排序
+    const ITEM = ref({
+        "categoryid":4,
+        "search":"",
+        "ordering":"-spotid",
+        "page":2,
+        "page_size":9
+        })
+    const SPOTS = ref([])  //接收API回傳的結果
     const BASE_URL = import.meta.env.VITE_APIURL
     const API_URL = `${BASE_URL}/spots/`
-    const loadSpots = async() =>{
-        const response = await fetch(API_URL)
-        const datas = await response.json()
-        console.log(datas)
-        results.value = datas
-        console.log(results.value)
 
+    // 只要 ref() 響應式資料"改變" watchEffect就會被執行!! 
+    // 呼叫API 放在 watchEffect 裡面
+    watchEffect(async()=>{
+        const params = new URLSearchParams({
+            "categoryid":ITEM.value.categoryid,
+            "search":ITEM.value.search,
+            "ordering":ITEM.value.ordering,
+            "page":ITEM.value.page,
+            "page_size":ITEM.value.page_size,
+        })
+        // console.log(params.toString())
+        const URL_PARAMS = `${API_URL}?${params.toString()}`
+
+        const response = await fetch(URL_PARAMS)
+        const datas = await response.json()
+        // console.log(datas)
+        SPOTS.value = datas
+        // console.log(SPOTS.value)
+    })
+    // ?categoryid=3&search=&ordering=-spotid&page=2&page_size=9
+
+    // page 會由子組件傳過來
+    // 分頁功能
+    const pagingHandlder = page=>{
+        ITEM.value.page = page
     }
-    loadSpots()
+
 </script>
 
 <template>
+    <PagingComponent :totalPages="SPOTS.total_page" @goPaging="pagingHandlder"></PagingComponent>
     <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div v-for="{spotid, spottitle, spotdescription, spotimage, address} in results"  class="col">
+        <div v-for="{spotid, spottitle, spotdescription, spotimage, address} in SPOTS.results" :key="spotid" class="col">
             <div class="card h-100">
             <img :src="spotimage" class="card-img-top" :alt="spottitle">
             <div class="card-body">
-                <h5 class="card-title">{{ spottitle }}</h5>
+                <h5 class="card-title">{{ spotid }} {{ spottitle }}</h5>
                 <p class="card-text">{{ spotdescription.length <=100 ? spotdescription : spotdescription.substring(0,100) }}...</p>
             </div>
             <div class="card-footer">
